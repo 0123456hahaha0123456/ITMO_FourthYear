0aea83e40de712bc358b13ef9682e607611f62de
Trần Trung Đức <ductran.tpc@gmail.com>
2020-10-31 03:45:18 +0300

    Add test sign in web

---
+++ SoftwareTesting/lab3/src/test/java/URL.java
@@ -0,0 +1,11 @@
+public class URL {
+    public static String chrome = "src\\test\\resources\\chromedriver.exe";
+
+    public static String getChrome() {
+        return chrome;
+    }
+
+    public static void setChrome(String chrome) {
+        URL.chrome = chrome;
+    }
+}
